8b5336620873cf35426e7b5773b84ad477d417f9
Garron Haun <thefsm93@gmail.com>
2023-03-07 10:15:57 -0800

    Updating to the latest BigQuery SQL

--- org-level-recommedations-hub/flatten-table-bigquery.sql
+++ org-level-recommedations-hub/flatten-table-bigquery.sql
@@ -8,65 +8,52 @@
 # Some project names are missing
 ## *** Final Select ***
 select 
-  # These are the final columns used in Datastudio
   project_name,
   project_id,
   asset_type,
   name as recommender_name,
   location,
   REPLACE(recommender_subtype, "_", " ") as recommender_subtype,
-  # If we dont use distinct here, sum calculations could get messed up in datastudio. Applies to all array_aggs in this query. 
   ARRAY_AGG(distinct target_resource) as target_resources,
   last_refresh_time as recommender_last_refresh_time,
   primary_impact.category as impact_category,
+  if(primary_impact.cost_projection.cost.units is null,false, true) as has_impact_cost,
   ABS(primary_impact.cost_projection.cost.units) as impact_cost_unit,
   primary_impact.cost_projection.cost.currency_code as impact_currency_code,
   state as recommender_state,
   ARRAY_AGG(distinct folder_id ignore nulls) as folder_ids,
   ARRAY_AGG(distinct insight_id) as insight_ids,
   ARRAY_AGG(STRUCT(insight_name, insight_type, insight_subtype, category as insight_category, insight_state)) as insights
-  ## *** Join Query to build initial table *** 
   from (
     select * except(associated_insights, target_resources)
     # We just want to grab the latest refresh time per export
-    from 
-    ## *** Query to grab only the LATEST recommendations ***
-    (SELECT
+    from (SELECT
         agg.table.*
       FROM (
-        SELECT
-          name,
-          # Small note for anyone who comes after me
-          # ARRAY_AGGs are cool because you can order the results,
-          # and then grab as many of the results as you want. 
-          # In this case we are grabbing on the newest result. 
-          ARRAY_AGG(STRUCT(table)
-          ORDER BY
-            last_refresh_time DESC)[SAFE_OFFSET(0)] agg
+        select target_resource,
+        recommender_subtype,
+            ARRAY_AGG(STRUCT(table)
+              ORDER BY  
+                last_refresh_time DESC)[SAFE_OFFSET(0)] agg
         FROM
-          `finOps.recommendations_export` table
-        GROUP BY
-          name)
+          `recommender-export-377819.recommendations_export_dataset.recommendations_export` table cross join unnest(target_resources) as target_resource
+          GROUP BY target_resource, recommender_subtype)
     ) as r
-    # Need to unnest fields for joins
     cross join unnest(associated_insights) as insight_id
     cross join unnest(target_resources) as target_resource
-    # Cross joins will remove nulls, and in our case we still need nulls
+    #Cross join will remove nulls, and in our case we still need nulls
     left join unnest(ancestors.folder_ids) as folder_id
-    ### Join Cloud Assets
     left join  (
       select 
       REGEXP_EXTRACT(name, r'/([^/]+)/?$') as project_name,
       REGEXP_EXTRACT(ancestor,  r'/([^/]+)/?$') as project_id,
       asset_type from 
-      (select * from `finOps.cloudAssets` 
+      (select * from `recommender-export-377819.recommendations_export_dataset.asset_export_table`
       cross join unnest(ancestors) as ancestor
       where asset_type in ("compute.googleapis.com/Project")
       and ancestor like "projects/%")
     ) as a
     on r.cloud_entity_id = a.project_id
-    ### Join insights
-    # Currently there is a bug where VM resizing recommendations are missing associated insights. This should be fixed soon. 
     left join (select 
       name as insight_name,
       insight_type,
@@ -74,10 +61,10 @@ select
       category,
       state as insight_state,
       a_r
-      from finOps.insights_export
+      from `recommender-export-377819.recommendations_export_dataset.insights_export`
       cross join unnest(associated_recommendations) as a_r
     ) as i
     on r.name=i.a_r
   )
-  group by 1,2,3,4,5,6,8,9,10,11,12
+  group by 1,2,3,4,5,6,8,9,10,11,12,13
   order by recommender_name
