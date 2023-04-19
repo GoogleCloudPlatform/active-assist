ccb96e7f012f673ae1f1b4d4e984c0c2ab270d63
Garron Haun <thefsm93@gmail.com>
2023-04-19 13:45:49 -0700

    Removing Asset Type from the flattened table due to it potentially creating duplicate rows, and removing because we don't use it anywhere

--- org-level-recommedations-hub/flatten-table-bigquery.sql
+++ org-level-recommedations-hub/flatten-table-bigquery.sql
@@ -1,16 +1,6 @@
-# This is a bigquery script designed to be used in Data Studio to properly display the data.
-# Keep in mind this script is currently under development, and probably is not optimized or in a final state. 
-# The reason this script was created is because you need to flatten individual recommendations to single rows if you want to make dashboards in Datastudio efficiently
-# I also am selecting individual columns to help increase the speed in Datastudio. 
-# Known issues:
-# Currently we dont handle additional currencies well.
-# Folder_ids do not map to folder names (missing from asset inventory)
-# Some project names are missing
-## *** Final Select ***
 select 
   project_name,
   project_id,
-  asset_type,
   name as recommender_name,
   location,
   REPLACE(recommender_subtype, "_", " ") as recommender_subtype,
@@ -43,16 +33,22 @@ select
     cross join unnest(target_resources) as target_resource
     #Cross join will remove nulls, and in our case we still need nulls
     left join unnest(ancestors.folder_ids) as folder_id
-    left join  (
-      select 
-      REGEXP_EXTRACT(name, r'/([^/]+)/?$') as project_name,
-      REGEXP_EXTRACT(ancestor,  r'/([^/]+)/?$') as project_id,
-      asset_type from 
-      (select * from `recommender-export-377819.recommendations_export_dataset.asset_export_table`
-      cross join unnest(ancestors) as ancestor
-      where asset_type in ("compute.googleapis.com/Project")
-      and ancestor like "projects/%")
-    ) as a
+    left join (Select project_name, project_id from
+      (
+        select 
+        if(
+              ENDS_WITH(name, "/billingInfo"),
+              REGEXP_EXTRACT(REPLACE(name,"/billingInfo",""), r'/([^/]+)/?$'),
+              REGEXP_EXTRACT(name, r'/([^/]+)/?$')
+        ) as project_name,
+        REGEXP_EXTRACT(ancestor,  r'/([^/]+)/?$') as project_id,
+        asset_type from 
+          (select * from `recommender-export-377819.recommendations_export_dataset.asset_export_table`
+          cross join unnest(ancestors) as ancestor
+          where asset_type in ("compute.googleapis.com/Project", "cloudbilling.googleapis.com/ProjectBillingInfo")
+          and ancestor like "projects/%")
+      )
+     ) as a
     on r.cloud_entity_id = a.project_id
     left join (select 
       name as insight_name,
@@ -66,5 +62,5 @@ select
     ) as i
     on r.name=i.a_r
   )
-  group by 1,2,3,4,5,6,8,9,10,11,12,13
+  group by 1,2,3,4,5,7,8,9,10,11,12
   order by recommender_name
