be6a675d6073ea836d9ae1d4cefcf51c31900abd
Garron Haun <thefsm93@gmail.com>
2023-05-04 14:12:32 -0700

    Moving main query to struct for better handling

--- org-level-recommedations-hub/ticketservice/internal/ticketinterfaces/baseTicketInterface.go
+++ org-level-recommedations-hub/ticketservice/internal/ticketinterfaces/baseTicketInterface.go
@@ -1,17 +1,17 @@
-``` Copyright 2020 Google LLC
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.```
 
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
-
-      http://www.apache.org/licenses/LICENSE-2.0
-
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.```
- 
 package ticketinterfaces
 
 import (
@@ -46,6 +46,19 @@ type Ticket struct {
 	SnoozeDate      time.Time `json:"snoozeDate"`
 	Subject         string    `json:"subject"`
 	Assignee        []string    `json:"assignee"`
+}
+
+
+type RecommendationQueryResult struct {
+	Project_name	string	
+	Project_id	string
+	Recommender_name	string
+	Location	string
+	Recommender_subtype	string
+	Impact_cost_unit	int
+	Impact_currency_code	string
+	TargetResource	string
+	Ticket	Ticket
 }
 
 func InitTicketService(implName string) (BaseTicketService, error) {
--- org-level-recommedations-hub/ticketservice/ticketLogic.go
+++ org-level-recommedations-hub/ticketservice/ticketLogic.go
@@ -1,16 +1,16 @@
-``` Copyright 2020 Google LLC
-
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
-
-      http://www.apache.org/licenses/LICENSE-2.0
-
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.```
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.```
 
 package main
 
@@ -18,13 +18,14 @@ import (
 	"fmt"
 	"regexp"
 	"strings"
+	"reflect"
 	b "ticketservice/internal/bigqueryfunctions"
 	"ticketservice/internal/ticketinterfaces"
 	u "ticketservice/internal/utils"
 	"time"
 
-	"github.com/mitchellh/mapstructure"
 )
+
 
 var nonAlphanumericRegex = regexp.MustCompile(`[^a-zA-Z0-9 ]+`)
 
@@ -33,7 +34,28 @@ var nonAlphanumericRegex = regexp.MustCompile(`[^a-zA-Z0-9 ]+`)
 // %[3] is the Cost Threshold
 // %[4] is an additional string added to allow null values
 // TODO: (GHAUN) reduce the number of returned fields
-var checkQueryTpl = `SELECT * EXCEPT(insights, insight_ids, target_resources)
+var checkQueryTpl = `SELECT f.* EXCEPT(
+	recommender_last_refresh_time,
+	has_impact_cost,
+	recommender_state,
+	folder_ids,
+	insights,
+	insight_ids,
+	target_resources),
+	TargetResource,
+	struct(
+			IFNULL(t.IssueKey, "") as IssueKey,
+			IFNULL(t.TargetContact, "") as TargetContact,
+			IFNULL(t.CreationDate, TIMESTAMP '1970-01-01T00:00:00Z') as CreationDate,
+			IFNULL(t.Status, "") as Status,
+			IFNULL(t.TargetResource, "") as TargetResource,
+			IFNULL(t.RecommenderID, "") as RecommenderID,
+			IFNULL(t.LastUpdateDate, TIMESTAMP '1970-01-01T00:00:00Z') as LastUpdateDate,
+			IFNULL(t.LastPingDate, TIMESTAMP '1970-01-01T00:00:00Z') as LastPingDate,
+			IFNULL(t.SnoozeDate, TIMESTAMP '1970-01-01T00:00:00Z') as SnoozeDate,
+			IFNULL(t.Subject, "") as Subject,
+			t.Assignee
+			) as Ticket
 	FROM %[1]s as f 
 	cross join unnest(target_resources) as TargetResource 
 	Left Join %[2]s as t 
@@ -42,6 +64,7 @@ var checkQueryTpl = `SELECT * EXCEPT(insights, insight_ids, target_resources)
 	(impact_cost_unit >= %[3]d %[4]s) 
 	and recommender_subtype not in (%[5]s)
 	limit 1` // This is temporary.
+
 
 func checkAndCreateNewTickets() error {
 	var allowNullString string
@@ -56,29 +79,29 @@ func checkAndCreateNewTickets() error {
 		c.ExcludeSubTypes,
 	)
 	u.LogPrint(1, "Querying for new Tickets")
-	results, err := b.QueryBigQueryToMap(query)
+	t := reflect.TypeOf(ticketinterfaces.RecommendationQueryResult{})
+	results, err := b.QueryBigQueryToStruct(query, t)
 	if err != nil {
 		u.LogPrint(4,"Failed to query bigquery for new tickets")
 		return err
 	}
 	var rowsToInsert []ticketinterfaces.Ticket
-	for _, row := range results{
-		ticket := ticketinterfaces.Ticket{}
-		if err := mapstructure.Decode(row, &ticket); err != nil{
-			u.LogPrint(3,"Failed to decode row into ticket")
-			return err
+	for _, r := range results{
+		row, ok := r.(ticketinterfaces.RecommendationQueryResult);
+		if !ok {
+			return fmt.Errorf("Failed to convert Query Schema into RecommendationQueryResults")
 		}
-		recommenderID := fmt.Sprintf("%s",row["recommender_name"])
+		ticket := row.Ticket
 		// Logic for if the ticket is already created
 		if ticket.IssueKey != ""{
 			u.LogPrint(3,"Already Exists: " + ticket.IssueKey)
-			ticket.RecommenderID = recommenderID
+			ticket.RecommenderID = row.Recommender_name
 			ticket.SnoozeDate = time.Now().AddDate(0,0,7)
 			rowsToInsert = append(rowsToInsert, ticket)
 			continue;
 		}
 		u.LogPrint(1, "Retrieving Routing Information")
-		routingRows, err := b.GetRoutingRowsByProjectID(c.BqRoutingTable,fmt.Sprintf("%v", row["project_id"]))
+		routingRows, err := b.GetRoutingRowsByProjectID(c.BqRoutingTable,row.Project_id)
 		if err != nil {
 			u.LogPrint(3,"Failed to get routing information")
 			return err
@@ -86,8 +109,8 @@ func checkAndCreateNewTickets() error {
 		u.LogPrint(1,"Creating new Ticket")
 		ticket.TargetContact = routingRows[0].Target
 		// And then adding to BQ Table.
-		lastSlashIndex := strings.LastIndex(ticket.TargetResource, "/")
-		secondToLast := strings.LastIndex(ticket.TargetResource[:lastSlashIndex], "/")
+		lastSlashIndex := strings.LastIndex(row.TargetResource, "/")
+		secondToLast := strings.LastIndex(row.TargetResource[:lastSlashIndex], "/")
 		// Update the fields of the ticket that need updating from the map
 		ticket.CreationDate = time.Now()
 		ticket.LastUpdateDate = time.Now()
@@ -98,12 +121,12 @@ func checkAndCreateNewTickets() error {
 		// We also need to combine target resource with recommender subtype
 		// This may not be the best format....but it works for now
 		ticket.Subject = fmt.Sprintf("%s-%s",
-				row["recommender_subtype"],
+				row.Recommender_subtype,
 				nonAlphanumericRegex.ReplaceAllString(
-					ticket.TargetResource[secondToLast+1:],
+					row.TargetResource[secondToLast+1:],
 					""))
 		ticket.Assignee = routingRows[0].TicketSystemIdentifiers
-		ticket.RecommenderID = recommenderID
+		ticket.RecommenderID = row.Recommender_name
 		// I need a way to catch IF a ticket is already created
 		ticketID, err := ticketService.CreateTicket(ticket)
 		if err != nil {
