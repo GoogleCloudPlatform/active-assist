45124124e02b9448666771fe005c1b443c9f10a9
Garron Haun <thefsm93@gmail.com>
2023-08-03 18:53:02 +0000

    Small Schema file fixes

--- recommendations-dashboard/terraform/modules/recommendations-dashboard/bigquery_schemas/recommendations_export_schema.sql
+++ recommendations-dashboard/terraform/modules/recommendations-dashboard/bigquery_schemas/recommendations_export_schema.sql
@@ -4,7 +4,7 @@
   {"description":"Name of recommendation. A project recommendation is represented as\\nprojects/[PROJECT_NUMBER]/locations/[LOCATION]/recommenders/[RECOMMENDER_ID]/recommendations/[RECOMMENDATION_ID]\\n","mode":"NULLABLE","name":"name","type":"STRING"},
   {"description":"Location for which this recommendation is generated\\n","mode":"NULLABLE","name":"location","type":"STRING"},
   {"description":"Recommender ID of the recommender that has produced this recommendation\\n","mode":"NULLABLE","name":"recommender","type":"STRING"},
-  {"description":"Contains an identifier for a subtype of recommendations produced for the\\nsame recommender. Subtype is a function of content and impact, meaning a\\nnew subtype will be added when either content or primary impact category\\nchanges.\\nExamples:\\nFor recommender = \\"google.iam.policy.Recommender\\",\\nrecommender_subtype can be one of \\"REMOVE_ROLE\\"/\\"REPLACE_ROLE\\"\\n","mode":"NULLABLE","name":"recommender_subtype","type":"STRING"},
+  {"description":"Contains an identifier for a subtype of recommendations produced for the\\nsame recommender. Subtype is a function of content and impact, meaning a\\nnew subtype will be added when either content or primary impact category\\nchanges.\\nExamples:\\nFor recommender = \"google.iam.policy.Recommender\",\\nrecommender_subtype can be one of \"REMOVE_ROLE\"/\"REPLACE_ROLE\"\\n","mode":"NULLABLE","name":"recommender_subtype","type":"STRING"},
   {"description":"Contains the fully qualified resource names for resources changed by the\\noperations in this recommendation. This field is always populated. ex:\\n[//cloudresourcemanager.googleapis.com/projects/foo].\\n","mode":"REPEATED","name":"target_resources","type":"STRING"},
   {"description":"Required. Free-form human readable summary in English.\\nThe maximum length is 500 characters.\\n","mode":"NULLABLE","name":"description","type":"STRING"},
   {"description":"Output only. Last time this recommendation was refreshed by the system that created it in the first place.\\n","mode":"NULLABLE","name":"last_refresh_time","type":"TIMESTAMP"},
@@ -13,7 +13,7 @@
     {"description":"Optional. Use with CategoryType.COST","fields":[
       {"description":"An approximate projection on amount saved or amount incurred.\\nNegative cost units indicate cost savings and positive cost units indicate\\nincrease. See google.type.Money documentation for positive/negative units.\\n","fields":[
         {"description":"The 3-letter currency code defined in ISO 4217.","mode":"NULLABLE","name":"currency_code","type":"STRING"},
-        {"description":"The whole units of the amount. For example if `currencyCode` is `\\"USD\\"`,\\nthen 1 unit is one US dollar.\\n","mode":"NULLABLE","name":"units","type":"INTEGER"},
+        {"description":"The whole units of the amount. For example if `currencyCode` is `\"USD\"`,\\nthen 1 unit is one US dollar.\\n","mode":"NULLABLE","name":"units","type":"INTEGER"},
         {"description":"Number of nano (10^-9) units of the amount.\\nThe value must be between -999,999,999 and +999,999,999 inclusive.\\nIf `units` is positive, `nanos` must be positive or zero.\\nIf `units` is zero, `nanos` can be positive, zero, or negative.\\nIf `units` is negative, `nanos` must be negative or zero.\\nFor example $-1.75 is represented as `units`=-1 and `nanos`=-750,000,000.\\n","mode":"NULLABLE","name":"nanos","type":"INTEGER"}
       ],"mode":"NULLABLE","name":"cost","type":"RECORD"},
       {"description":"Duration for which this cost applies.","fields":[
